9f739aedb787d9654298e907fcebcc40212bcdab
静雨·安婵 <102129419+IonRh@users.noreply.github.com>
2025-12-14 17:27:26 +0800

    Add files via upload
    增加docker

---
+++ Docker/main.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export TZ='Asia/Shanghai'
+
+# 检查/root是否有config.json文件
+if [ -f /root/config.json ]; then
+    # 有文件则执行BestIP
+    cd /root && ./BestIP > /dev/null 2>&1 &
+else
+	# 无文件则移动/app/到/root
+	mv /app/config.json /root/
+    mv /app/TGBot_RSS /root/
+    cd /root/
+    echo "修改config.json重启Docker后，正常运行"
+    # ./TGBot_RSS > /dev/null 2>&1 &
+fi
+
+tail -f /dev/null
