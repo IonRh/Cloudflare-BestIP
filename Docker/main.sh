9e0449e519c47b98604cd160735e5824dee921f1
静雨·安婵 <102129419+IonRh@users.noreply.github.com>
2026-02-01 18:43:53 +0800

    Update main.sh

--- Docker/main.sh
+++ Docker/main.sh
@@ -5,7 +5,7 @@ export TZ='Asia/Shanghai'
 # 检查/root是否有config.json文件
 if [ -f /root/config.json ]; then
     # 有文件则执行BestIP
-    cd /root && ./BestIP > /dev/null 2>&1 &
+    cd /root && ./BestIP
 else
 	# 无文件则移动/app/到/root
 	mv /app/config.json /root/
